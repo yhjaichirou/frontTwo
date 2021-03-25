0cf5fc3ca31b07b6626a55670174aded2268f65d
yhjaichirou <835057159@qq.com>
2021-03-25 08:02:10 +0800

    强强强强

--- vue-element-admin-master/vue.config.js
+++ vue-element-admin-master/vue.config.js
@@ -7,6 +7,8 @@ function resolve(dir) {
 }
 
 const name = defaultSettings.title || 'vue Element Admin' // page title
+//const CompressionWebpackPlugin = require('compression-webpack-plugin') //引入插件
+//const productionGzipExtensions = ['js', 'css'] //压缩js 和css文件
 
 // If your port is set to 80,
 // use administrator privileges to execute the command line.
@@ -24,7 +26,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/',
+  publicPath: './',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
@@ -37,15 +39,15 @@ module.exports = {
       errors: true
     },
     //before: require('./mock/mock-server.js'),
-	proxy: {
-	  [process.env.VUE_APP_BASE_API]: {
-		target: `http://localhost:8088/`,
-		changeOrigin: true,
-		pathRewrite: {
-		// "/admin":"/project/admin"
-		}
-	  }
-	}
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: `http://localhost:8088/`,
+        changeOrigin: true,
+        pathRewrite: {
+          // "/admin":"/project/admin"
+        }
+      }
+    }
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
@@ -57,18 +59,47 @@ module.exports = {
       }
     }
   },
+  // configureWebpack: {
+  //   // webpack plugins
+  //   plugins: [
+  //     // Ignore all locale files of moment.js
+  //     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+  //     // GitRevision.version()
+  //     new webpack.DefinePlugin({
+  //       APP_VERSION: `"${require('./package.json').version}"`,
+  //       GIT_HASH: JSON.stringify('1.2'),
+  //       BUILD_DATE: buildDate
+  //     }),
+
+  //     // 配置压缩
+  //     new CompressionWebpackPlugin({
+  //       algorithm: 'gzip',
+  //       test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+  //       threshold: 10240,
+  //       minRatio: 0.8
+  //     }),
+  //     //maxChunks：使用大于或等于 1 的值，来限制 chunk 的最大数量。使用 1 防止添加任何其他额外的 chunk，这是因为entry/main chunk 也会包含在计数之中。
+  //     //minChunkSize: 设置 chunk 的最小大小。
+  //     //在合并 chunk 时，webpack 会尝试识别出具有重复模块的 chunk，并优先进行合并。任何模块都不会被合并到 entry   chunk 中，以免影响初始页面加载时间。
+  //     new webpack.optimize.LimitChunkCountPlugin({
+  //       maxChunks: 5,
+  //       minChunkSize: 100
+  //     })
+  //   ],
+  //   // if prod, add externals
+  //   externals: isProd ? assetsCDN.externals : {}
+  // },
+  
   chainWebpack(config) {
     // it can improve the speed of the first screen, it is recommended to turn on preload
     // it can improve the speed of the first screen, it is recommended to turn on preload
-    config.plugin('preload').tap(() => [
-      {
-        rel: 'preload',
-        // to ignore runtime.js
-        // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
-        fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
-        include: 'initial'
-      }
-    ])
+    config.plugin('preload').tap(() => [{
+      rel: 'preload',
+      // to ignore runtime.js
+      // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
+      fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
+      include: 'initial'
+    }])
 
     // when there are many pages, it will cause too many meaningless requests
     config.plugins.delete('prefetch')
@@ -97,7 +128,7 @@ module.exports = {
             .plugin('ScriptExtHtmlWebpackPlugin')
             .after('html')
             .use('script-ext-html-webpack-plugin', [{
-            // `runtime` must same as runtimeChunk name. default is `runtime`
+              // `runtime` must same as runtimeChunk name. default is `runtime`
               inline: /runtime\..*\.js$/
             }])
             .end()
